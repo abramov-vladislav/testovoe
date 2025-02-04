f62ef5ecb9f2736f6f2f544d79349d6399f84421
icvlad <euphorils@yandex.ru>
2025-02-04 13:34:12 +0300

    тестовое задание + заметки по проекту (по возможной реализации)

--- src/test/java/org/abramov/spring/testovoe/TestovoeApplicationTests.java
+++ src/test/java/org/abramov/spring/testovoe/TestovoeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TestovoeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
