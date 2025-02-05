dc8c9c66b118e61df73457878ea8ad712f4ef330
icvlad <euphorils@yandex.ru>
2025-02-05 18:14:33 +0300

    05.02.2025 TaskController.java

--- user-service/src/main/java/org/abramov/spring/testovoe/userservice/repository/UserRepository.java
+++ user-service/src/main/java/org/abramov/spring/testovoe/userservice/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package org.abramov.spring.testovoe.userservice.repository;
 import org.abramov.spring.testovoe.userservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, UUID> {
+
     Optional<User> findByUserId (UUID userId);
 }
