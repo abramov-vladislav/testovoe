6589b59a7c5e27d08b1667086f99382820dc13be
icvlad <euphorils@yandex.ru>
2025-02-05 14:46:35 +0300

    05.02.2025 TaskServiceImpl

--- task-service/src/main/java/org/abramov/spring/testovoe/taskservice/dto/TaskDTO.java
+++ task-service/src/main/java/org/abramov/spring/testovoe/taskservice/dto/TaskDTO.java
@@ -1,5 +1,34 @@
 package org.abramov.spring.testovoe.taskservice.dto;
 
-//DTO (Data Transfer Objects) – объекты для передачи данных
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.abramov.spring.testovoe.taskservice.model.Task;
+import org.abramov.spring.testovoe.userservice.dto.UserDTO;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
 public class TaskDTO {
+    private UUID taskId;
+    private String taskName;
+    private UserDTO taskOwner; // Владелец - пользователь
+    private UserDTO taskExecutor; // Исполнитель - пользователь
+    private Task.TaskStatus taskStatus;
+    private LocalDateTime taskCreateDate;
+    private LocalDateTime taskUpdateDate;
+
+    // Метод для преобразования Task в TaskDto
+    public static TaskDTO from(Task task, UserDTO owner, UserDTO executor) {
+        return new TaskDTO(
+                task.getTaskId(),
+                task.getTaskName(),
+                owner, // Передаем информацию о владельце
+                executor, // Передаем информацию об исполнителе
+                task.getTaskStatus(),
+                task.getTaskCreateDate(),
+                task.getTaskUpdateDate()
+        );
+    }
 }
--- user-service/src/main/java/org/abramov/spring/testovoe/userservice/dto/UserDTO.java
+++ user-service/src/main/java/org/abramov/spring/testovoe/userservice/dto/UserDTO.java
@@ -1,5 +1,22 @@
 package org.abramov.spring.testovoe.userservice.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.util.UUID;
 
 //DTO (Data Transfer Objects) – объекты для передачи данных
+@Data
+@AllArgsConstructor
 public class UserDTO {
+    private UUID userId;
+    private String userLogin;
+    private String fullName; // Конкатенация фамилии и имени
+
+    // Метод преобразования User в UserDto
+    public static UserDTO from(org.abramov.spring.testovoe.userservice.model.User user) {
+        return new UserDTO(
+                user.getUserId(),
+                user.getUserLogin(),
+                user.getUserLastName() + " " + user.getUserName() // Объединяем фамилию и имя
+        );
+    }
 }
