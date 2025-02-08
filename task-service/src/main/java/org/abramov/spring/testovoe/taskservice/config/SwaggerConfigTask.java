bd009c06674471bd2d9032d7cd342d370c66bc44
harbertw home <euphorils@yandex.ru>
2025-02-08 23:13:36 +0300

    swagger

---
+++ task-service/src/main/java/org/abramov/spring/testovoe/taskservice/config/SwaggerConfigTask.java
@@ -0,0 +1,6 @@
+package org.abramov.spring.testovoe.taskservice.config;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfigTask {
+}
---
+++ user-service/src/main/java/org/abramov/spring/testovoe/userservice/config/SwaggerConfigUser.java
@@ -0,0 +1,7 @@
+package org.abramov.spring.testovoe.userservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfigUser {
+}
