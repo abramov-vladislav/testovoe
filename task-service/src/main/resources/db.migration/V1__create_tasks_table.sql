1dc48a00ea1d166cf202eee4f68b6866633dba94
abramov <euphorils@yandex.ru>
2025-04-22 17:15:32 +0300

    доработки

--- task-service/src/main/resources/db.migration/V1__create_tasks_table.sql
+++ task-service/src/main/resources/db.migration/V1__create_tasks_table.sql
@@ -11,15 +11,21 @@ CREATE TYPE task_service.status AS ENUM (
 
 CREATE TABLE IF NOT EXISTS task_service.tasks
 (
-    task_id          UUID PRIMARY KEY,
-    task_name        VARCHAR(255) NOT NULL,
-    owner_user_id    UUID         NOT NULL,
-    executor_user_id UUID         NOT NULL,
-    task_status      VARCHAR(20),
+    task_id UUID PRIMARY KEY,
+    task_name VARCHAR(255) NOT NULL,
+    owner_user_id UUID,
+    executor_user_id UUID,
+    task_status VARCHAR(20),
     task_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     task_update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_owner FOREIGN KEY (owner_user_id) REFERENCES user_service.users (user_id) ON DELETE CASCADE,
-    CONSTRAINT fk_executor FOREIGN KEY (executor_user_id) REFERENCES user_service.users (user_id) ON DELETE CASCADE
+    CONSTRAINT fk_owner FOREIGN KEY (owner_user_id) REFERENCES task_service.user_task (user_id) ON DELETE CASCADE, --удалить задачи, при удалении пользователя
+    CONSTRAINT fk_executor FOREIGN KEY (executor_user_id) REFERENCES task_service.user_task (user_id) ON DELETE CASCADE --удалить задачи, при удалении пользователя
 );
 
-
+CREATE TABLE IF NOT EXISTS task_service.user_task
+(
+    user_id UUID PRIMARY KEY,
+    username VARCHAR NOT NULL,
+    user_last_name VARCHAR NOT NULL,
+    user_first_name VARCHAR NOT NULL
+);
